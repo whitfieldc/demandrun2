7aaa4aa0e57017ceba986b329e6d0bbcbe174827
Charles Whitfield <whitfield.cw@gmail.com>
2016-01-13 23:23:00 -0800

    convert to postgresql

--- db/migrate/20160114054445_create_runners.rb
+++ db/migrate/20160114054445_create_runners.rb
@@ -2,8 +2,8 @@ class CreateRunners < ActiveRecord::Migration
   def change
     create_table :runners do |t|
       t.string :name
-      t.string :short
-      t.string :long
+      t.integer :short
+      t.integer :long
 
       t.timestamps null: false
     end
--- db/migrate/20160114063522_convert_distances_to_integers.rb
+++
@@ -1,6 +0,0 @@
-class ConvertDistancesToIntegers < ActiveRecord::Migration
-  def change
-  	change_column :runners, :short, :integer
-  	change_column :runners, :long, :integer
-  end
-end
